ccfe8ea3a7ebafc995d8b941113925b16f94686f
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-12 16:29:25 +0200

    using the token for request to protected resources && change password functionality has implemented

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,43 @@
 import classes from './ProfileForm.module.css';
+import {useContext, useRef} from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+    const newPasswordInputRef = useRef();
+    const authCtx = useContext(AuthContext);
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredNewPassword = newPasswordInputRef.current.value;
+
+        // add validation
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBbXkzQTebW8z_RrNjwLGnlkWSJY9L4EdQ', {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: authCtx.token,
+                password: enteredNewPassword,
+                returnSecureToken: false
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                // assumption: Always succeeds!
+
+            });
+    }
+    return (
+        <form onSubmit={submitHandler} className={classes.form}>
+            <div className={classes.control}>
+                <label htmlFor='new-password'>New Password</label>
+                <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef}/>
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
 }
 
 export default ProfileForm;
