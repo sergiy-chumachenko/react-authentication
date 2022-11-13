4cb5dbccf32e15082c56fda3c91537e270bea168
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-13 16:56:31 +0200

    protecting frontend pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import { Switch, Route } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import {useContext} from "react";
+import AuthContext from "./store/auth-context";
+import Redirect from "react-router-dom/es/Redirect";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+    const authCtx = useContext(AuthContext);
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <HomePage/>
+                </Route>
+                {!authCtx.isLoggedIn && (
+                    <Route path='/auth'>
+                        <AuthPage/>
+                    </Route>
+                )}
+                <Route path='/profile'>
+                    {authCtx.isLoggedIn && <UserProfile/>}
+                    {!authCtx.isLoggedIn && <Redirect to='/auth'/>}
+                </Route>
+                <Route path='*'>
+                    <Redirect to='/'/>
+                </Route>
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
